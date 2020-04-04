847cd0f8afd1ad51b546d737626b7f95eb34ced8
ahmettinastepe <ahmettinastepe0@gmail.com>
2020-04-04 19:05:32 +0300

    Add project files.

---
+++ TemelMvc/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TemelMvc.Models;
+
+namespace TemelMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new Book());
+        }
+
+        public ActionResult Detail()
+        {
+            string description = "Bu bir açıklamadır......!";
+
+            return View(model: description);
+        }
+    }
+}
---
+++ TemelMvc/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TemelMvc.Models
+{
+    public class Book
+    {
+        public string Name { get; set; } = "C# İle Nesne Tabanlı Programlama";
+        public int PageSize { get; set; } = 186;
+    }
+}
